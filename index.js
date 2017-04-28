184801dfc3f6940a0211afe037095d664feb3512
Ammar Hasan <ammar@marketlytics.com>
2017-04-29 01:47:00 +0500

    Post user status change

---
+++ index.js
@@ -0,0 +1,42 @@
+var Botkit = require('botkit')
+
+var token = process.env.SLACK_TOKEN
+
+var controller = Botkit.slackbot({
+  // reconnect to Slack RTM when connection goes bad
+  retry: Infinity,
+  debug: false
+})
+
+// Assume single team mode if we have a SLACK_TOKEN
+if (token) {
+  console.log('Starting in single-team mode')
+  controller.spawn({
+    token: token
+  }).startRTM(function (err, bot, payload) {
+    if (err) {
+      throw new Error(err)
+    }
+
+    console.log('Connected to Slack RTM')
+  })
+// Otherwise assume multi-team mode - setup beep boop resourcer connection
+} else {
+  console.log('Starting in Beep Boop multi-team mode')
+  require('beepboop-botkit').start(controller, { debug: true })
+}
+
+var channel;
+controller.on('bot_channel_join', function (bot, message) {
+  channel = message.channel;
+  bot.reply(message, "I'm here!")
+})
+
+controller.on('user_change', function (bot, message) {
+  var status = message.user.profile;
+  var name = message.user.real_name;
+  bot.say({
+    text: name + 'is ' + status.status_text + ' ' + status.status_emoji,
+    channel: channel
+  });
+})
